fdf7edcdbbb0a205d425eb38776b48fe8a708b9d
sneh-soni <ssneh20062003@gmail.com>
2024-03-02 19:31:25 +0530

    client setup

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CssBaseline } from "@mui/material";
+import { HelmetProvider } from "react-helmet-async";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <CssBaseline />
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
